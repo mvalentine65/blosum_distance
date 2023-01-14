22a57db1eb4e49776239e42605f7461ae8df9a02
Chubak-ENIAC <chubak-eniac@pop-os.localdomain>
2023-01-14 08:17:05 +0330

    made it work with small files

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,8 @@
 #![allow(unused)]
 
 extern crate bio;
-extern crate pyo3;
 extern crate hashbrown;
-
+extern crate pyo3;
 
 use core::num;
 use std::borrow::BorrowMut;
@@ -15,15 +14,14 @@ use std::io::{prelude::*, BufReader};
 //use bio::alphabets::dna::complement;
 use bio::io::fasta;
 use bio::scores::blosum62;
-use std::collections::{HashMap, HashSet};
 use itertools::Itertools;
-use pyo3::prelude::*;
-use std::io::IoSliceMut;
 use proc_utils::{add_x_members, repeat_across_x_times, repeat_across_x_times_and_create_struct};
+use pyo3::prelude::*;
+use std::collections::{HashMap, HashSet};
+use std::io::IoSliceMut;
 use std::ptr::addr_of_mut;
 
 // use rayon::prelude::*;
-
 
 // Lifetime annotations for object lifetime c
 // and some lifetime s which is greater than c.
@@ -45,23 +43,21 @@ use std::ptr::addr_of_mut;
 //     dupe_count: u8,
 // }
 
-
-macro_rules! str_make  {
+macro_rules! str_make {
     ($s:literal) => {
         String::from($s)
     };
 }
 
-
 macro_rules! hm_make {
     (
-        $key_type:ty, 
-        $value_type:ty, 
+        $key_type:ty,
+        $value_type:ty,
         $($key:expr => $value:expr), *) => {
             {
                 let mut hm = HashMap::<$key_type, $value_type>::new();
 
-                $ 
+                $
                 (
                     hm.insert($key, $value);
                 )
@@ -69,16 +65,16 @@ macro_rules! hm_make {
 
                 hm
             }
-            
+
     };
     (
-        $key_type:ty, 
-        $value_type:ty, 
+        $key_type:ty,
+        $value_type:ty,
         $($key:literal => $value:literal), *) => {
             {
                 let mut hm = HashMap::<$key_type, $value_type>::new();
 
-                $ 
+                $
                 (
                     hm.insert($key, $value);
                 )
@@ -86,10 +82,9 @@ macro_rules! hm_make {
 
                 hm
             }
-            
+
     };
 }
-
 
 #[pyfunction]
 fn find_references_and_candidates(path: String) -> (Vec<String>, Vec<String>) {
@@ -103,26 +98,23 @@ fn find_references_and_candidates(path: String) -> (Vec<String>, Vec<String>) {
         line = line_in.unwrap();
         if reached_candidates {
             candidates.push(line);
-        }
-        else if line.starts_with('>') && !line.ends_with('.') {
+        } else if line.starts_with('>') && !line.ends_with('.') {
             reached_candidates = true;
             candidates.push(line)
-        }
-        else {
+        } else {
             references.push(line)
         }
     }
     (references, candidates)
 }
 
-
 #[pyfunction]
 fn make_indices(sequence: String) -> (usize, usize) {
     let start: usize = sequence
         .chars()
         .enumerate()
-        .filter(|(_,bp)| *bp != '-')
-        .map(|(index, _)|index)
+        .filter(|(_, bp)| *bp != '-')
+        .map(|(index, _)| index)
         .take(1)
         .next()
         .unwrap();
@@ -130,14 +122,13 @@ fn make_indices(sequence: String) -> (usize, usize) {
         .chars()
         .rev()
         .enumerate()
-        .filter(|(_,bp)| *bp != '-')
-        .map(|(index, _)|index)
+        .filter(|(_, bp)| *bp != '-')
+        .map(|(index, _)| index)
         .take(1)
         .next()
         .unwrap();
-    (start, sequence.len()-delta_end)
+    (start, sequence.len() - delta_end)
 }
-
 
 #[pyfunction]
 fn fasta_reader(path: String) -> Vec<String> {
@@ -145,7 +136,7 @@ fn fasta_reader(path: String) -> Vec<String> {
     let fasta_reader = fasta::Reader::from_file(path).unwrap();
     for fasta in fasta_reader.records() {
         let record = &fasta.unwrap();
-        result.push(format!(">{}",record.id()));
+        result.push(format!(">{}", record.id()));
         result.push(String::from_utf8(record.seq().to_vec()).unwrap());
     }
     result
@@ -172,14 +163,16 @@ fn cluster_distance_filter(lines: Vec<&str>) -> Vec<Vec<&str>> {
         clusters.entry(key).or_insert(Vec::new()).push(array);
     }
     let mut output = Vec::new();
-    for (_key, mut cluster) in clusters{
+    for (_key, mut cluster) in clusters {
         // Pops Hamming records off the cluster vector and makes the new record
         // the leader in a HammingCluster. Compares leader to other values by distance
         // and
-        loop{
+        loop {
             // if we pop a None, the vector is empty, so break
             let lead = cluster.pop();
-            if lead.is_none() { break;}
+            if lead.is_none() {
+                break;
+            }
             let mut lead = lead.unwrap();
             // if lead.is_none() { continue; }
             // let group = HammingCluster {
@@ -197,24 +190,20 @@ fn cluster_distance_filter(lines: Vec<&str>) -> Vec<Vec<&str>> {
                 let candidate = &cluster[index];
                 // if candidate.is_none() {continue;}
                 // let other = candidate.unwrap();
-                if seqs_within_distance(candidate[1],
-                                        lead[1],
-                                        1) {
+                if seqs_within_distance(candidate[1], lead[1], 1) {
                     // group.others.push(other)
                     lead.extend(cluster.swap_remove(index));
                 }
             }
             output.push(lead)
-        };
+        }
     }
     output
 }
 
 #[pyfunction]
 fn batch_reverse_complement(list: Vec<String>) -> Vec<String> {
-    list.into_iter()
-        .map(bio_revcomp)
-        .collect()
+    list.into_iter().map(bio_revcomp).collect()
 }
 
 #[pyfunction]
@@ -225,20 +214,23 @@ fn bio_revcomp(sequence: String) -> String {
 #[pyfunction]
 fn seqs_within_distance(first: &str, second: &str, max_distance: u32) -> bool {
     let (array_one, array_two) = (first.as_bytes(), second.as_bytes());
-    if array_one.len() != array_two.len() { return false; }
+    if array_one.len() != array_two.len() {
+        return false;
+    }
     let mut distance: u32 = 0;
     for i in 0..array_one.len() {
         if array_one[i] != array_two[i] {
             distance += 1;
-            if distance > max_distance { return false}
+            if distance > max_distance {
+                return false;
+            }
         }
     }
     true
-} 
+}
 
 #[add_x_members(buff_*: Vec<u8>, => 256)]
-struct Buffers { }
-
+struct Buffers {}
 
 impl Buffers {
     pub fn new() -> Self {
@@ -256,14 +248,15 @@ struct FastLineReader {
     bufreader: BufReader<File>,
     iovecs: Vec<IoSliceMut<'static>>,
     str_buffers: Vec<String>,
-    remaining: usize,
+    remainder: String,
+    remaining_vec: Vec<usize>,
 }
 
 impl FastLineReader {
     pub fn new(path: String, buffers_ptr: *mut Buffers) -> Self {
         println!("Starting new FastLineReader instance on path {path}");
-        
-        let buffers: &mut Buffers= unsafe { &mut *buffers_ptr };
+
+        let buffers: &mut Buffers = unsafe { &mut *buffers_ptr };
 
         let bufreader = BufReader::new(File::open(path).expect("Error opening output file"));
 
@@ -272,45 +265,76 @@ impl FastLineReader {
         repeat_across_x_times! {
             iovecs.push(IoSliceMut::new(&mut buffers.buff_*)); @ 256
         };
-        
+
         let str_buffers = vec![str_make!(""); 256];
 
-        Self { bufreader, iovecs, str_buffers, remaining: 0usize }
-
+        Self {
+            bufreader,
+            iovecs,
+            str_buffers,
+            remaining_vec: vec![],
+            remainder: str_make!(""),
+        }
     }
-    
-    pub fn fill_and_get_lines(&mut self, index: usize) -> Vec<String> {
-        let mut vec_str = Vec::<String>::new();
-        let iovec = self.iovecs.get(index).expect("Index exceeds for iovecs");
-        let buffer = self.str_buffers.get_mut(index).expect("Error getting string buffer");
 
-        iovec
-            .iter()
-            .cloned()
-            .for_each(|c| {
-                if c == 10 {
-                    vec_str.push(buffer.clone());
-                    buffer.clear();
-                }
+    pub fn fill_and_get_lines(&mut self, index: usize) {
+        let curr_buffer = self
+            .str_buffers
+            .get_mut(index)
+            .expect("Error getting buffer");
+        let curr_vio = self.iovecs.get(index).expect("Error getting vecio");
 
-                buffer.push(c as char);
-            });
-
-        vec_str
+        curr_vio.iter().cloned().for_each(|c| {
+            curr_buffer.push(c as char);
+        });
     }
 
     pub fn read_batch(&mut self) -> Vec<String> {
-        self.remaining = self.bufreader.read_vectored(&mut self.iovecs).expect("Error with Vector Read");
-        
+        let rem = self
+            .bufreader
+            .read_vectored(&mut self.iovecs)
+            .expect("Error with Vector Read");
+
+        self.remaining_vec.push(rem);
+
+        if rem == 0 {
+            if self.remainder.len() != 0 {
+                return vec![self.remainder.clone()];
+            }
+
+            return vec![];
+        }
+
         let ret_list = (0..256)
-                                .into_iter()
-                                .map(|i| self.fill_and_get_lines(i))
-                                .flatten()
-                                .collect::<Vec<_>>();
-                                
+            .into_iter()
+            .for_each(|i| self.fill_and_get_lines(i));
 
+        let strings_together = self.str_buffers.join("");
+        let with_rem = format!("{}{}{}", self.remainder, strings_together, "\n");
 
-        ret_list
+        let mut split: Vec<String> = vec![];
+        let mut buff = str_make!("");
+
+        with_rem.chars().for_each(|c| {
+            if c == '\n' {
+                let trimmed = buff.trim();
+                split.push(trimmed.to_string());
+                buff.clear();
+            } else {
+                if c != '\0' {
+                    buff.push(c);
+                }
+            }
+        });
+
+        if *self.remaining_vec.first().unwrap() == 8192 * 256
+            && self.remaining_vec.len() > 1
+            && *self.remaining_vec.last().unwrap() == 8192 * 256
+        {
+            self.remainder = split.pop().unwrap();
+        }
+
+        split
     }
 }
 
@@ -324,7 +348,10 @@ struct VecIO {
 impl VecIO {
     #[new]
     pub fn new() -> Self {
-        Self { buffers: Buffers::new(), freader: None }
+        Self {
+            buffers: Buffers::new(),
+            freader: None,
+        }
     }
 
     pub fn init(&mut self, path: String) {
@@ -332,13 +359,13 @@ impl VecIO {
 
         self.freader = Some(FastLineReader::new(path, buffers_ptr));
     }
-    
+
     pub fn is_done(&mut self) -> bool {
         let derefed = self.freader.as_mut().unwrap();
 
-        derefed.remaining != 8192
+        *derefed.remaining_vec.last().unwrap() == 0
     }
-    
+
     pub fn get_next_batch(&mut self) -> Vec<String> {
         let derefed = self.freader.as_mut().unwrap();
 
@@ -360,47 +387,54 @@ fn not_skip_character(character: u8) -> bool {
     character != HYPHEN && character != ASTERISK
 }
 
-
 fn blosum62_check(a: u8, b: u8) -> i32 {
-    if a == 45 || b == 45 { return 0_i32; }
+    if a == 45 || b == 45 {
+        return 0_i32;
+    }
     blosum62(a, b)
 }
 
-
 fn blosum62_self_check(a: u8, b: u8) -> i32 {
-    if a == 45 || b == 45 { return 0_i32; }
-    blosum62(a,a)
+    if a == 45 || b == 45 {
+        return 0_i32;
+    }
+    blosum62(a, a)
 }
 
-
 fn self_distance_vectors(seq1: &str, seq2: &str) -> (Vec<i32>, Vec<i32>) {
-    (seq1.as_bytes().iter().zip(seq2.as_bytes().iter())
-        .scan(0_i32,|state, (a,b)| {
-            *state += blosum62_self_check(*a, *b);
-            Some(*state)
-        })
-        .collect(),
-        seq2.as_bytes().iter().zip(seq1.as_bytes().iter())
-            .scan(0_i32, | state, (a , b)| {
+    (
+        seq1.as_bytes()
+            .iter()
+            .zip(seq2.as_bytes().iter())
+            .scan(0_i32, |state, (a, b)| {
                 *state += blosum62_self_check(*a, *b);
                 Some(*state)
             })
-            .collect())
+            .collect(),
+        seq2.as_bytes()
+            .iter()
+            .zip(seq1.as_bytes().iter())
+            .scan(0_i32, |state, (a, b)| {
+                *state += blosum62_self_check(*a, *b);
+                Some(*state)
+            })
+            .collect(),
+    )
 }
-
 
 #[pyfunction]
 fn make_ref_distance_vector_blosum62(seq1: &str, seq2: &str) -> (Vec<i32>, (Vec<i32>, Vec<i32>)) {
-    let numerator = seq1.as_bytes().iter()
+    let numerator = seq1
+        .as_bytes()
+        .iter()
         .zip(seq2.as_bytes().iter())
         .scan(0, |state, (a, b)| {
             *state += blosum62_check(*a, *b);
             Some(*state)
         })
         .collect();
-        (numerator, self_distance_vectors(seq1,seq2))
+    (numerator, self_distance_vectors(seq1, seq2))
 }
-
 
 #[pyfunction]
 fn make_ref_distance_matrix_supervector(refs: Vec<&str>) -> Vec<(Vec<i32>, (Vec<i32>, Vec<i32>))> {
@@ -411,39 +445,51 @@ fn make_ref_distance_matrix_supervector(refs: Vec<&str>) -> Vec<(Vec<i32>, (Vec<
         .collect()
 }
 
-
-fn extract_distance(cross_distance: &Vec<i32>, max_vec1: &Vec<i32>, max_vec2: &Vec<i32>, start_index: usize, stop_index: usize) -> f64 {
+fn extract_distance(
+    cross_distance: &Vec<i32>,
+    max_vec1: &Vec<i32>,
+    max_vec2: &Vec<i32>,
+    start_index: usize,
+    stop_index: usize,
+) -> f64 {
     let denominator = match start_index {
-        0 => std::cmp::max(max_vec1[stop_index-1], max_vec2[stop_index-1]),
-        _ => std::cmp::max(max_vec1[stop_index-1] - max_vec1[start_index-1], max_vec2[stop_index-1] - max_vec2[start_index-1])
+        0 => std::cmp::max(max_vec1[stop_index - 1], max_vec2[stop_index - 1]),
+        _ => std::cmp::max(
+            max_vec1[stop_index - 1] - max_vec1[start_index - 1],
+            max_vec2[stop_index - 1] - max_vec2[start_index - 1],
+        ),
     };
     let numerator = match start_index {
-        0 => cross_distance[stop_index-1],
-        _ => cross_distance[stop_index-1] - cross_distance[start_index-1]
+        0 => cross_distance[stop_index - 1],
+        _ => cross_distance[stop_index - 1] - cross_distance[start_index - 1],
     };
     1.0_f64 - (numerator as f64 / denominator as f64)
 }
 
-
 #[pyfunction]
-fn ref_index_vector (supervector: Vec<(Vec<i32>, (Vec<i32>, Vec<i32>))>, start: usize, end: usize) -> Vec<f64> {
-    supervector.iter()
+fn ref_index_vector(
+    supervector: Vec<(Vec<i32>, (Vec<i32>, Vec<i32>))>,
+    start: usize,
+    end: usize,
+) -> Vec<f64> {
+    supervector
+        .iter()
         .map(|(cross, (max1, max2))| extract_distance(cross, max1, max2, start, end))
         .collect()
 }
 
-
-
 #[pyfunction]
-fn blosum62_distance(one: String, two: String) -> PyResult<f64>{
+fn blosum62_distance(one: String, two: String) -> PyResult<f64> {
     let first: &[u8] = one.as_bytes();
     let second: &[u8] = two.as_bytes();
     let mut score = 0;
     let mut max_first = 0;
     let mut max_second = 0;
     let length = first.len();
-    let allowed: HashSet<u8> = HashSet::from([65,84,67,71,73,68,82,
-        80,87,77,69,81,83,72,86,76,75,70,89,78,88, 90, 74, 66,  79, 85]);
+    let allowed: HashSet<u8> = HashSet::from([
+        65, 84, 67, 71, 73, 68, 82, 80, 87, 77, 69, 81, 83, 72, 86, 76, 75, 70, 89, 78, 88, 90, 74,
+        66, 79, 85,
+    ]);
     for i in 0..length {
         // if !(first[i] == HYPHEN || second[i] == HYPHEN) {
         if not_skip_character(first[i]) && not_skip_character(second[i]) {
@@ -458,15 +504,13 @@ fn blosum62_distance(one: String, two: String) -> PyResult<f64>{
             max_first += bio::scores::blosum62(first[i], first[i]);
             max_second += bio::scores::blosum62(second[i], second[i]);
         }
-    };
+    }
     // println!("score: {}", score);
     // println!("max_first: {}", max_first);
     // println!("max_second: {}", max_second);
     let maximum_score = std::cmp::max(max_first, max_second);
     Ok(1.0 - (score as f64 / maximum_score as f64))
 }
-
-
 
 // A Python module implemented in Rust.
 #[pymodule]

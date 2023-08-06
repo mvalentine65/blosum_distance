e70c7cd9a999368b2eeb4b8cb654f77667c4cafc
Michael Valentine <mvalentine65@gmail.com>
2023-08-05 21:25:23 -0400

    adds final loop for single decrements of end

--- src/lib.rs
+++ src/lib.rs
@@ -144,34 +144,31 @@ fn make_location_data(letters: &[u32;27]) -> LocationData {
 }
 
 fn _excise_consensus_tail(consensus: &String, limit: f64) -> (String, usize) {
-    let mut step = 16_usize;
+    let step = 16_usize;
     let mut percent_invalid = 1.0_f64;
     let mut num_invalid = 0;
     let mut denominator = 0;
     let length = consensus.len();
     let mut end = length;
     let mut start = length - step;
-    while percent_invalid > limit {
+    while start >= step && percent_invalid > limit {
         num_invalid += consensus[start..end].bytes().filter(|&x| x == b'X').count();
         denominator += step;
         percent_invalid = num_invalid as f64 / denominator as f64;
         if percent_invalid < limit {
             break;
         }
-        if start == 0 && percent_invalid > limit {
-            break;
-        } // prevent underflow
-        if start < 16 {
-            end -= start;
-            step = start;
-            start = 0;
-        } else {
-            start -= step;
-            end -= step;
-        }
+        start -= step;
+        end -= step;
     }
-    // single character pass on last block to find a more exact location
-
+    while end > 0 && end < usize::MAX && percent_invalid > limit{
+        end -= 1;
+        if &consensus[end..end+1] == "X"{
+            num_invalid += 1;
+        }
+        denominator += 1;
+        percent_invalid = num_invalid as f64 / denominator as f64;
+    }
     (consensus[0..end].to_string(), end)
 }
 
